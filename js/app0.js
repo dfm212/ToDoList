ead61efc6af45ab26dc9c28297ae4a1dab464a29
dfm212 <daniel_ve14@hotmail.com>
2015-08-14 20:59:31 -0430

    a ver

---
+++ js/app0.js
@@ -0,0 +1,105 @@
+(function(){
+
+  var app = angular.module('IndexPost', []);
+
+  app.controller('AddFileController', function(){
+    this.file = {};
+
+    this.addFile  = function(product){
+        product.files.push(this.file);
+      this.file = {};
+    };
+  });
+
+  var websites = {
+    folder: [],
+    init: function() {
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDom: function() {
+      this.$el = $('#wallModule');
+      this.$buttonAddFolder = this.$el.find('button.add');
+      this.$buttonInputAddFolder = this.$el.find('button.newFolder');
+      this.$buttonCancelFolder = this.$el.find('button.cancel');
+      this.$buttonAddboard = this.$el.find('button.newBoard');
+      this.$input = this.$el.find('input');
+      this.$ol = this.$el.find('ol');
+      this.template = this.$el.find('{{folders.folder}}').html();
+    },
+    bindEvents: function(){
+      this.$buttonInputAddFolder.on('click', this.show.bind(this));
+      this.$buttonAddFolder.on('click', this.addFolder.bind(this));
+      this.$buttonAddboard.on('click', this.addBoard.bind(this));
+      this.$ol.delegate('i.del', 'click', this.deleteFolder.bind(this));
+      this.$buttonCancelFolder.on('click', this.hideInputFolder.bind(this));
+    },
+    render: function(){
+      var data = {
+        folder: this.folder,
+      };
+      this.$ol.html(Mustache.render(this.template, data));
+    },
+    addFolder: function(){
+      if(this.$input.val().length >= 1){
+        this.folder.push(this.$input.val());
+        this.render();
+        this.$input.val('');
+
+        $('.hiden').addClass('hide');
+        $('.newFolder').removeClass('hide');
+      }
+    },
+    show: function(){
+      var toShow = $(".newFolder").on('click', function(){
+        $(this).addClass('hide');
+        $('.hiden').removeClass('hide');
+      });
+    },
+    hideInputFolder: function(){
+      var cancel = $('button.cancel').on('click', function(){
+        $('.hiden ').addClass('hide');
+        $('.newFolder').removeClass('hide');
+      });
+      this.render();
+    },
+    addBoard: function(){
+      var toAddBoard = $("#newBoard").click(function(){
+
+      });      // var toAddBoard = $("#newBoard").click(function(){
+      //     $("#boardsModule").append($("#board").clone(false));
+      // });
+      this.render();
+    },
+
+    deleteFolder: function(event){
+
+      var $remove = $(event.target).closest('li');
+      var i = this.$ol.find('li').index($remove);
+
+      this.folder.splice(i, 1);
+      this.render();
+    }
+
+  };
+  folders.init();
+
+  $('.boardTitle').bind('dblclick', function(){
+        $(this).attr('contentEditable',true);
+    });
+
+
+
+
+  // app.directive("websitesData", function() {
+  //   return {
+  //     restrict: 'E',
+  //     templateUrl: "websites.html"
+  //   };
+  // });
+
+
+
+
+})();
